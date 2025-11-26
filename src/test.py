8dad60c93a1af3567d08ee75f556bdc53c4911fd
Jose Carlos <2530064@upv.edu.mx>
2025-11-26 15:56:21 -0600

    feat new accomodations

--- src/test.py
+++ src/test.py
@@ -1,24 +1,23 @@
-print("welcome to the debt elegibility system")
-while True:#mientras qeu mis condiciones no se cumplan el codigo se repetira
-    monthly_income=input("set your income\n")
-    monthly_debt = input("set your monthly debt studio\n")
-    credit_score = input("set your creditscore")
-
-    try:#prueba que las entradas si sean compatibles con los datos requeridos
-        income=float(monthly_income)
-        debt=float(monthly_debt)
-        score=int(credit_score)
-        if income>0 and debt>=0 and score>=0:
-            break
-        else:
-            print("Error: invalid input.") 
-    except ValueError:#mensajes de error por formato incorrecto
-        print("please insert a valid input")
-debt_ratio=debt/income#calculo para el debt ratio
-if income >=8000.0 and debt_ratio<=.4 and score>=650:#se busca por las condiciones de elgibilidad
-    elegible=True
-else:
-    elegible= False
-#salidas    
-print(f"Debt ratio: {round(debt_ratio,2)}")
-print(f"Eligible: {elegible}")
+print("welcome to the statistics calculator")
+numbers=[]#inicializamos una lista
+counter=0#definimos variables que nos serviran mas tarde
+while True:#nos pedira que volvamos a ingresar los numeros hasta que estos sean enteros
+    num1 = input("please set your first \n")
+    num2 = input("please set your second \n")
+    num3 = input("please set your third \n")
+    try:#verificamos que los numeros sean enteros
+        nat1= int(num1)
+        nat2= int(num2)
+        nat3= int(num3)
+        numbers.extend([nat1,nat2,nat3])#agregamos todo a numbers
+        break #rompe el ciclo si las condiciones se cumplen
+    except ValueError:
+        print("error:invalid number")#mensaje de error si la entrada no es valor numerico
+for number in numbers:#compara si los valores son pares
+    if number%2==0:
+        counter=counter+1
+print(f"sum:{sum(numbers)}")#imprime la suma de los numeros
+print(f"average:{(sum(numbers))/3}")#imprime el promedio de los numeros
+print(f"maximum value:{max(numbers)}")#imprime el valor maximo de los numeros
+print(f"minimum value: {min(numbers)}")# imprime el valor minimo de los numeros
+print(f"all even: {counter==3}")#nos dice si todos los valores son pares
